7da9537c18ad2e92389b9859038f52ddfc11a929
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-18 17:25:07 -0700

    write and pass getdescription test

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -11,8 +11,21 @@ namespace HairSalon.Tests
         [TestMethod]
         public void ClientConstructor_CreatesInstanceOfClient_Client()
         {
-            Client newClient = new Client("test");
+            Client newClient = new Client("Ada");
             Assert.AreEqual(typeof(Client), newClient.GetType());
+        }
+        [TestMethod]
+        public void GetDescription_ReturnsDescription_String()
+        {
+            //Arrange
+            string description = "Ada";
+
+            //Act
+            Client newClient = new Client(description);
+            string result = newClient.Description;
+
+            //Assert
+            Assert.AreEqual(description, result);
         }
     }
 }
