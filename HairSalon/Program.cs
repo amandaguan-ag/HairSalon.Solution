486a35e871c096ecf0da49dcd256d254ebcf1b1a
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-20 14:20:50 -0700

    add shared css

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -3,27 +3,26 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ProjectName
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-      builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();
 
-      WebApplication app = builder.Build();
+            WebApplication app = builder.Build();
 
-      // app.UseDeveloperExceptionPage();
-      app.UseHttpsRedirection();
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            // app.UseStaticFiles();
+            app.UseRouting();
 
-      app.UseRouting();
-
-      app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
-
-      app.Run();
+            app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+            app.Run();
+        }
     }
-  }
 }
