f115cbdd5c405b741d1d1a4abf2f6f582b1a9fde
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-20 14:52:43 -0700

    setup efcore and remove stylist model and test directory

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,32 +1,122 @@
-using System.Collections.Generic;
-
 namespace HairSalon.Models
 {
     public class Client
-{
-    public string Description { get; set; }
-    public int Id { get; }
-    private static List<Client> _instances = new List<Client> { };
+    {
+        public int ClientId { get; set; }
+        public string Description { get; set; }
 
-    public Client(string description)
-    {
-        Description = description;
-        _instances.Add(this);
-        Id = _instances.Count;
-    }
+        // public Client(string description)
+        // {
+        //   Description = description;
+        // }
+        // public Client(string description, int id)
+        // {
+        //   Description = description;
+        //   Id = id;
+        // }
 
-    public static List<Client> GetAll()
-    {
-        return _instances;
-    }
+        // public override bool Equals(System.Object otherClient)
+        // {
+        //   if (!(otherClient is Client))
+        //   {
+        //     return false;
+        //   }
+        //   else
+        //   {
+        //     Client newClient = (Client) otherClient;
+        //     bool idEquality = (this.Id == newClient.Id);
+        //     bool descriptionEquality = (this.Description == newClient.Description);
+        //     return (idEquality && descriptionEquality);
+        //   }
+        // }
 
-    public static void ClearAll()
-    {
-        _instances.Clear();
+        // public void Save()
+        // {
+        //   MySqlConnection conn = DB.Connection();
+        //   conn.Open();
+        //   MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+
+        //   cmd.CommandText = "INSERT INTO clients (description) VALUES (@ClientDescription);";
+        //   MySqlParameter param = new MySqlParameter();
+        //   param.ParameterName = "@ClientDescription";
+        //   param.Value = this.Description;
+        //   cmd.Parameters.Add(param);    
+        //   cmd.ExecuteNonQuery();
+        //   Id = (int) cmd.LastInsertedId;
+
+        //   conn.Close();
+        //   if (conn != null)
+        //   {
+        //     conn.Dispose();
+        //   }
+        // }
+
+        // public static Client Find(int id)
+        // {
+        //   MySqlConnection conn = DB.Connection();
+        //   conn.Open();
+
+        //   MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        //   cmd.CommandText = "SELECT * FROM `clients` WHERE id = @ThisId;";
+
+        //   MySqlParameter param = new MySqlParameter();
+        //   param.ParameterName = "@ThisId";
+        //   param.Value = id;
+        //   cmd.Parameters.Add(param);
+
+        //   MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        //   int clientId = 0;
+        //   string clientDescription = "";
+        //   while (rdr.Read())
+        //   {
+        //     clientId = rdr.GetInt32(0);
+        //     clientDescription = rdr.GetString(1);
+        //   }
+        //   Client foundClient = new Client(clientDescription, clientId);
+
+        //   conn.Close();
+        //   if (conn != null)
+        //   {
+        //     conn.Dispose();
+        //   }
+        //   return foundClient;
+        // }
+
+        // public static List<Client> GetAll()
+        // {
+        //   List<Client> allClients = new List<Client> { };
+        //   MySqlConnection conn = DB.Connection();
+        //   conn.Open();
+        //   MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        //   cmd.CommandText = "SELECT * FROM clients;";
+        //   MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        //   while (rdr.Read())
+        //   {
+        //       int clientId = rdr.GetInt32(0);
+        //       string clientDescription = rdr.GetString(1);
+        //       Client newClient = new Client(clientDescription, clientId);
+        //       allClients.Add(newClient);
+        //   }
+        //   conn.Close();
+        //   if (conn != null)
+        //   {
+        //       conn.Dispose();
+        //   }
+        //   return allClients;
+        // }
+
+        // public static void ClearAll()
+        // {
+        //   MySqlConnection conn = DB.Connection();
+        //   conn.Open();
+        //   MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        //   cmd.CommandText = "DELETE FROM clients;";
+        //   cmd.ExecuteNonQuery();
+        //   conn.Close();
+        //   if (conn != null)
+        //   {
+        //    conn.Dispose();
+        //   }
+        // }
     }
-    public static Client Find(int searchId)
-    {
-        return _instances[searchId - 1];
-    }
-}
 }
