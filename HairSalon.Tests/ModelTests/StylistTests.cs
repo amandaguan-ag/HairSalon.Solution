96385394f3e0b8c7969dcea1fd18e54626a5bdea
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-19 17:03:33 -0700

    write and pass getname test

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -14,5 +14,18 @@ namespace HairSalon.Tests
             Stylist newStylist = new Stylist("test stylist");
             Assert.AreEqual(typeof(Stylist), newStylist.GetType());
         }
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            //Arrange
+            string name = "Test Stylist";
+            Stylist newStylist = new Stylist(name);
+
+            //Act
+            string result = newStylist.Name;
+
+            //Assert
+            Assert.AreEqual(name, result);
+        }
     }
 }
