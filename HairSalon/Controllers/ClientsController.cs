31d71c2d40bdd4a92aebe7c4c6073d039a976ce3
Amanda Guan <ag.amandaguan@gmail.com>
2023-05-20 15:12:57 -0700

    write create, read and detail with efcore

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,28 +1,42 @@
-// using Microsoft.AspNetCore.Mvc;
-// using HairSalon.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace HairSalon.Controllers
-// {
-//     public class ClientsController : Controller
-//     {
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly HairSalonContext _db;
 
-//         [HttpGet("/stylists/{stylistId}/clients/new")]
-//         public ActionResult New(int stylistId)
-//         {
-//             Stylist stylist = Stylist.Find(stylistId);
-//             return View(stylist);
-//         }
+        public ClientsController(HairSalonContext db)
+        {
+            _db = db;
+        }
 
-//         [HttpGet("/stylists/{stylistId}/clients/{clientId}")]
-//         public ActionResult Show(int stylistId, int clientId)
-//         {
-//             Client client = Client.Find(clientId);
-//             Stylist stylist = Stylist.Find(stylistId);
-//             Dictionary<string, object> model = new Dictionary<string, object>();
-//             model.Add("client", client);
-//             model.Add("stylist", stylist);
-//             return View(model);
-//         }
-//     }
-// }
+        public ActionResult Index()
+        {
+            List<Client> model = _db.Clients.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Clients.Add(client);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+    }
+}
